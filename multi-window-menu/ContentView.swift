20d0c6a8890a854dc5d0a3fdb55e897fa36ae5e9
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2021-07-03 22:43:24 -0700

    Initial Commit

---
+++ multi-window-menu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  multi-window-menu
+//
+//  Created by Ondrej Kvasnovsky on 7/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ multi-window-menu/multi_window_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_window_menuApp.swift
+//  multi-window-menu
+//
+//  Created by Ondrej Kvasnovsky on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct multi_window_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
